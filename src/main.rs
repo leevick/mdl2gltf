d23d7d4c720738400c4f573e06541369f06bdf97
leevick <leevick@tsinghua.edu.cn>
2022-04-22 20:47:16 +0800

    test:Experience RUST features

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::thread;
+use std::time::Duration;
+
+mod blender {}
+mod fsx {}
+
 fn main() {
-    println!("Hello, world!");
+    let r = json::parse(r#"{"abc":"def"}"#);
+    println!("Hello, world! {:?}", r.unwrap()["abc"]);
+
+    let f = thread::spawn(|| {
+        for i in 1..10 {
+            thread::sleep(Duration::from_secs(1));
+            println!("{}", i);
+        }
+    });
+
+    f.join().unwrap();
 }
