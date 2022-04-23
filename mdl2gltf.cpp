b3daf24320f7bf4a14ac70fb487123c542dcacae
leevick <leevick@tsinghua.edu.cn>
2022-04-23 19:52:45 +0800

    test: Rust lang features

---
+++ mdl2gltf.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
-use std::thread;
-use std::time::Duration;
+use std::env;
+use std::fs::File;
+use std::io::Read;
 
 mod blender {}
 mod fsx {}
 
+mod riff {
+    struct Section {
+        name: String,
+        offset: usize,
+        length: usize,
+    }
+}
+
+const RIFF_TAG: [&str; 35] = [
+    "RIFF", "MDLH", "MDLG", "MDLN", "SMAP", "PARA", "CRAS", "BBOX", "RADI", "MDLD", "TEXT", "MATE",
+    "INDE", "VERB", "VERT", "TANS", "BMAP", "SKIN", "TRAN", "AMAP", "SCEN", "SGAL", "SGVL", "SGJC",
+    "SGBR", "LODT", "LODE", "PART", "ANIB", "PLAL", "PLAT", "REFL", "REFP", "ATTO", "SHAM",
+];
+
 fn main() {
-    let r = json::parse(r#"{"abc":"def"}"#);
-    println!("Hello, world! {:?}", r.unwrap()["abc"]);
+    // let args: Vec<String> = env::args().collect();
+    // let file = File::open(&args[1]);
+    let file = File::open("data/Il-86.MDL");
+    let mut buffer = vec![0u8; 0x800000];
+    let len = file.unwrap().read(&mut buffer).unwrap();
+    println!("{}", len);
 
-    let f = thread::spawn(|| {
-        for i in 1..10 {
-            thread::sleep(Duration::from_secs(1));
-            println!("{}", i);
-        }
-    });
-
-    f.join().unwrap();
+    for t in RIFF_TAG {
+        println!("{}", t);
+    }
 }
